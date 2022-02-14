4602767ba2e8509109d69049facd29af5a958fcc
Anton Rodionov <91364221+AntonRodonow@users.noreply.github.com>
2022-02-14 22:47:44 +0300

    Update tasks.py

--- newapp/tasks.py
+++ newapp/tasks.py
@@ -21,37 +21,26 @@ datetime(2022, 5, 2, tzinfo=MSC)
 def weekly_digest():
     categories = Category.objects.all()
     today = DT.datetime.today()
-
-    week_ago = today - DT.timedelta(days=7)
     week = timedelta(days=7)
 
     print('today', today)
-    print('week_ago', week_ago)
-    print('week', week)
 
     for category in categories:
-        subscribers_emails = category.subscribers.all().values('email')
-        print('subscribers_emails', subscribers_emails)
-
         category_subscribers = category.subscribers.all()
         print('category_subscribers', category_subscribers)
-
         category_subscribers_emails = []
         for subscriber in category_subscribers:
             category_subscribers_emails.append(subscriber.email)
+        print('category_subscribers', category_subscribers_emails)
 
         weekly_posts_in_category = []
-        posts_in_category = Post.objects.all().filter(category=f'{category.id}')
+        posts_in_category = Post.objects.all().filter(postCategory=f'{category.id}')
 
         for post in posts_in_category:
             print('today = ', today)
-            print('post.d_time = ', post.d_time.utcnow())
-            # time_delta = DT.datetime.today - post.d_time
-            time_delta = DT.datetime.now() - post.d_time.utcnow()
-            print('43 time_delta', time_delta)
-            days_delta = today - post.d_time.utcnow()
-
-            print('45 days_delta', days_delta)
+            time_delta = DT.datetime.now() - post.dateCreation
+            print('days_delta', days_delta)
+            
             if time_delta < week:
                 weekly_posts_in_category.append(post)
                 print(f'Дата публикации: {post.d_time}')
@@ -59,7 +48,6 @@ def weekly_digest():
                 print('----------------   ---------------')
 
         print(f'ID: {category.id}')
-        print(category)
         print(f'Кол-во публикаций: {len(weekly_posts_in_category)}')
         print(category_subscribers_emails)
         print(weekly_posts_in_category)
